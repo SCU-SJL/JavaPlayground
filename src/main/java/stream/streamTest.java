1f4b89fa62819baf09b46d8c2aa620c528a366eb
shaojiale <sjl66666666@gmail.com>
2020-02-16 16:00:51 +0800

    Complete streamTest

--- src/main/java/stream/streamTest.java
+++ src/main/java/stream/streamTest.java
@@ -1,12 +1,15 @@
 package stream;
 
+import org.junit.Assert;
 import org.junit.Test;
 import pojo.User;
 
 import java.io.*;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -75,6 +78,10 @@ public class streamTest {
         stream.forEach(System.out::println);
     }
 
+    /**
+     * {@link Stream#sorted()} elements are supposed to implement {@link Comparable}
+     * {@link Stream#sorted(Comparator)} use the custom {@link Comparator} to sort the elements
+     */
     @Test
     public void sortOperation() {
         List<String> list = Arrays.asList("cc", "zz", "aa");
@@ -96,5 +103,72 @@ public class streamTest {
             }
             return user1.getName().compareTo(user2.getName());
         }).forEach(System.out::println);
+    }
+
+
+    /**
+     * Notice the difference between {@link Stream#map(Function)} and {@link Stream#peek(Consumer)},
+     * the first method requires a {@link Function}, which means it may has return value. But
+     * the last method requires a {@link Consumer}, which means it does not has return value.
+     */
+    @Test
+    public void peekOperation() {
+        User u1 = new User.Builder()
+                .name("Joe")
+                .age(41)
+                .build();
+        User u2 = new User.Builder()
+                .name("Bill")
+                .age(46)
+                .build();
+
+        List<User> userList = Arrays.asList(u1, u2);
+
+        userList.stream().peek(user -> user.setAge(18)).forEach(System.out::println);
+    }
+
+    @Test
+    public void aggregateOperation() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        Assert.assertFalse(list.stream().allMatch(e -> e > 10));
+        Assert.assertTrue(list.stream().noneMatch(e -> e < 1));
+        Assert.assertTrue(list.stream().anyMatch(e -> e == 5));
+
+        Assert.assertEquals(5, list.stream().count());
+        Assert.assertEquals(5, list.stream().max(Integer::compareTo).get().intValue());
+        Assert.assertEquals(1, list.stream().min(Integer::compareTo).get().intValue());
+    }
+
+    @Test
+    public void reduceOperation() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+
+        Assert.assertEquals(10, list.stream().reduce((n1, n2) -> n1 + n2).get().intValue());
+        Assert.assertEquals(110, list.stream().reduce(100, (n1, n2) -> n1 + n2).intValue());
+    }
+
+    @Test
+    public void collectOperation() {
+        User u1 = new User.Builder().name("Martin").age(19).build();
+        User u2 = new User.Builder().name("Marven").age(20).build();
+        User u3 = new User.Builder().name("Justin").age(21).build();
+        List<User> users = Arrays.asList(u1, u2, u3);
+
+        List<Integer> ageList = users.stream().map(User::getAge).collect(Collectors.toList());
+        Assert.assertEquals(3, ageList.size());
+        Assert.assertEquals(19, ageList.get(0).intValue());
+
+        Set<Integer> ageSet  = users.stream().map(User::getAge).collect(Collectors.toSet());
+        Assert.assertTrue(ageSet.contains(21));
+
+        Map<String, Integer> userMap = users.stream().collect(Collectors.toMap(User::getName, User::getAge));
+        Assert.assertEquals(20, userMap.get("Marven").intValue());
+
+        Assert.assertEquals(3, users.stream().collect(Collectors.counting()).intValue());
+        Assert.assertEquals(21, users.stream().map(User::getAge).collect(Collectors.maxBy(Integer::compareTo)).get().intValue());
+        Assert.assertEquals(60, users.stream().collect(Collectors.summarizingInt(User::getAge)).getSum());
+        Assert.assertEquals(20, users.stream().collect(Collectors.averagingInt(User::getAge)).intValue());
+
     }
 }
